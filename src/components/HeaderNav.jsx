1ac5f77fab8e1eb269147a31b6d30502d85d9aa2
priyanshu9026 <singpriyasnshu22@gmail.com>
2024-09-27 16:46:53 +0530

    initial commit

---
+++ src/components/HeaderNav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const HeaderNav = () => {
+  const nav = useNavigate();
+  const send = (e) => {
+    nav(e);
+  };
+  return (
+    <nav>
+      <div className="links">
+        <a href="https://facebook.com" target="blank">
+          <i class="fa fa-facebook-f i-fb"></i>
+        </a>
+        <a href="https://x.com" target="blank">
+          <i class="fa fa-twitter i-tw"></i>
+        </a>
+        <a href="https://linkedin.com" target="blank">
+          <i class="fa fa-linkedin i-ln"></i>
+        </a>
+
+        <a href="https://meta.com" target="blank">
+          <i class="fa fa-vine i-vn"></i>
+        </a>
+        <a href="https://instagram.com" target="blank">
+          <i class="fa fa-instagram i-ig"></i>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default HeaderNav;
